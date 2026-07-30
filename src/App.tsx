96f17e3c5d22aecd0ecd7736e1f6ffa492b32829
kingmohhe-spec <kingmohhe@gmail.com>
2026-07-30 16:24:02 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -242,9 +242,9 @@ function App() {
             </div>
 
             <h1 className="text-5xl md:text-7xl font-bold mb-6 leading-tight">
-              WHERE YOU SPEND LESS 
+              WHERE YOU SPEND LESS & 
               <span className="block bg-gradient-to-r from-orange-400 to-orange-600 bg-clip-text text-transparent">
-                & SAVEMORE
+                 SAVE-MORE
               </span>
             </h1>
 
