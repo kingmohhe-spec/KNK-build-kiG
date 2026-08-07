e0e686977322d49310a3cd67eb9386f60dc1edfb
kingmohhe-spec <kingmohhe@gmail.com>
2026-08-07 12:53:16 +0200

    Updated categoryDetails.ts

--- src/data/categoryDetails.ts
+++ src/data/categoryDetails.ts
@@ -103,7 +103,7 @@ export const categoryDetails: Record<string, CategoryProduct[]> = {
       image: 'https://images.pexels.com/photos/8491084/pexels-photo-8491084.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
     },
     {
-      name: 'Paints',
+      name: 'Paint Solutions',
       description: 'Ceiling insulation, Enamel Paint, Waterproofing Paint, Primers and Undercoats, PVA and Acrylic, Roofing paint',
       image: 'https://images.pexels.com/photos/4457372/pexels-photo-4457372.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
     },
